a30ae549a46392459e88d61284e00c266841a88d
James Phillips <james.phillips@bryanuniversity.edu>
2019-10-22 20:23:39 -0500

    Add Creating And Appending Elements

---
+++ Week 2/Creating And Appending Elements/index.js
@@ -0,0 +1,27 @@
+
+var newh1 = document.createElement("h1")
+newh1.textContent = "Welcome to my JS site"
+document.body.append(newh1)
+
+var newp = document.createElement("p")
+newp.textContent = "All of this was created with JavaScript"
+document.body.append(newp)
+
+var newul = document.createElement("ul")
+document.body.append(newul)
+
+var newli = document.createElement("li")
+newli.textContent = "Vegetables"
+document.body.append(newli)
+
+var newli = document.createElement("li")
+newli.textContent = "Cereal"
+document.body.append(newli)
+
+var newli = document.createElement("li")
+newli.textContent = "Milk"
+document.body.append(newli)
+
+var newli = document.createElement("li")
+newli.textContent = "Fruit"
+document.body.append(newli)
