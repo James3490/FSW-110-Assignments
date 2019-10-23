06d146c42198ffc2b6b4eca522e6a93cbd95320c
James Phillips <james.phillips@bryanuniversity.edu>
2019-10-23 15:16:52 -0500

    Adding Basic Website w/ JS

---
+++ Week 2/Basic Website wJS/index.js
@@ -0,0 +1,45 @@
+var header = document.createElement("header")
+header.textContent = "Basic Website w/ JS"
+document.body.append(header)
+
+var nav = document.createElement("nav")
+document.body.append(nav)
+
+var navul = document.createElement("ul")
+document.body.append(nav)
+
+var navli = document.createElement("li")
+navli.textContent = "Home"
+document.body.append(navli)
+
+var navli = document.createElement("li")
+navli.textContent = "Products"
+document.body.append(navli)
+
+var navli = document.createElement("li")
+navli.textContent = "Services"
+document.body.append(navli)
+
+var navli = document.createElement("li")
+navli.textContent = "About"
+document.body.append(navli)
+
+var navli = document.createElement("li")
+navli.textContent = "Contact"
+document.body.append(navli)
+
+var navli = document.createElement("li")
+navli.textContent = "Resources"
+document.body.append(navli)
+
+var h2 = document.createElement("h2")
+h2.textContent = "Main Section"
+document.body.append(h2)
+
+var p = document.createElement("p")
+p.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, a? Sint quasi facilis rem reiciendis sapiente nihil cum obcaecati nam, maiores cumque unde adipisci magnam mollitia. Minus, nam! Aliquam, distinctio autem quae debitis unde earum nesciunt eaque, eius laboriosam tempora, officiis accusamus. Dolores atque nostrum iste mollitia assumenda dolorum quo, itaque illo nihil aliquid unde quos? Placeat perspiciatis atque vitae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, a? Sint quasi facilis rem reiciendis sapiente nihil cum obcaecati nam, maiores cumque unde adipisci magnam mollitia. Minus, nam! Aliquam, distinctio autem quae debitis unde earum nesciunt eaque, eius laboriosam tempora, officiis accusamus."
+document.body.append(p)
+
+var footer = document.createElement("footer")
+footer.textContent = "This is my footer"
+document.body.append(footer)
