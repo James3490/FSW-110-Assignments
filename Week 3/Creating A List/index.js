16d27f9c5b44fa828decfdc909873a96e7361de6
James Phillips <james.phillips@bryanuniversity.edu>
2019-11-01 11:03:04 -0500

    Edited Assignment

--- Week 3/Creating A List/index.js
+++ Week 3/Creating A List/index.js
@@ -1,12 +1,3 @@
-    // document.write("<h1>Hello World</h1>")
-//     var i;
-//     var h1;
-//     const names = ["Steve", "Larry", "Joe", "Shirley", "Steph", "Nate", "Rick", "Emily"]
-//     for (i = 0; i < names.length; i++) {
-//     document.write("<h1>Hello World</h1>")
-// };
-//     var world = document.getElementsByTagName("h1")
-//     h1.innerHTML = "names[i]";
 var i;
     
     var names = ["Steve", "Larry", "Joe", "Shirley", "Steph", "Nate", "Rick", "Emily"]
