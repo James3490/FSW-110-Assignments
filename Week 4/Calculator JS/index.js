768b7320e70fee609102552de0ed2b156fee3402
James Phillips <james.phillips@bryanuniversity.edu>
2019-11-06 15:17:04 -0600

    Added Calculator JS Assignment

---
+++ Week 4/Calculator JS/index.js
@@ -0,0 +1,33 @@
+// var addends1 = document.getElementById("addends1").value;
+// var addends2=document.getElementById("addends2").value;
+// function addEventListeners() {}
+// document.getElementById("equals").addEventListener("click", sum);
+// var sum=parseFloat(addends1)+parseFloat("addends2")
+// function sum() {
+//     equals.document.getElementById("equals").innerHtml="The answer is" + additionsum;
+// }
+// var sum=parseFloat(addends1)+parseFloat("addends2")
+// var button = document.getElementById("additionsum")
+document.getElementById("calculate").addEventListener("click", function() {
+    document.getElementById("result").innerHTML = "Result: ";
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    console.log(num1, num2);
+    document.getElementById("result").innerHTML += (num1)+(num2)
+   })
+
+   document.getElementById("calculate2").addEventListener("click", function() {
+    document.getElementById("result").innerHTML = "Result: ";
+    var num3 = parseInt(document.getElementById("num3").value);
+    var num4 = parseInt(document.getElementById("num4").value);
+    console.log(num3, num4);
+    document.getElementById("result2").innerHTML += (num3)-(num4)
+   })
+
+   document.getElementById("calculate3").addEventListener("click", function() {
+    document.getElementById("result").innerHTML = "Result: ";
+    var num5 = parseInt(document.getElementById("num5").value);
+    var num6 = parseInt(document.getElementById("num6").value);
+    console.log(num5, num6);
+    document.getElementById("result3").innerHTML += (num5)*(num6)
+   })
